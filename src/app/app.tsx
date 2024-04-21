0e458f74de3b23f444411496a710531cca035c4b
XuanPhat <phatpx32@wru.vn>
2024-04-21 10:07:45 -0400

    Add lazy loading and not-found page

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,17 +2,22 @@
 import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
 import HomePage from './src/pages/home-page/HomePage';
 import MainLayout from './src/layouts/main-layout/MainLayout';
+import { Suspense, lazy } from 'react';
+import NotFoundPage from './src/pages/not-found-page/NotFoundPage';
 
 // import NxWelcome from './nx-welcome';
 
+const JobPage = lazy(() => import('./src/pages/job-page/JobPage'));
 const router = createBrowserRouter(
   createRoutesFromElements(
-    [
-      <Route path='/' Component={MainLayout}>
-        <Route index Component={HomePage}></Route>,
-        <Route path='/about' element= {<h1>About</h1>}></Route>
-      </Route>,
-    ])
+
+    <Route path='/' Component={MainLayout}>
+      <Route index element={<HomePage />}></Route>,
+      <Route path='/jobs' element={<Suspense fallback={<>...</>}><JobPage /></Suspense>}></Route>
+      <Route path='*' element={<NotFoundPage />}></Route>
+    </Route>
+
+    )
 );
 export function App() {
 
