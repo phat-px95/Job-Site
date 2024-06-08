e3f7dc8d58e6ad3258e2f74de450c209b4959ae6
XuanPhat <phatpx32@wru.vn>
2024-06-07 22:37:27 -0400

    Add transition group

--- src/app/src/components/job-listing/JobListing.tsx
+++ src/app/src/components/job-listing/JobListing.tsx
@@ -3,6 +3,7 @@ import Job from '../job/Job';
 import styles from './JobListing.module.css';
 import Spinner from '../spinner/Spinner';
 import { IJob } from '../../models/job.model';
+import { TransitionGroup } from 'react-transition-group';
 /* eslint-disable-next-line */
 export interface JobListingProps {
   isHome: boolean
@@ -28,21 +29,29 @@ export function JobListing({isHome = false}: JobListingProps) {
     fetchJobs();
   }, [])
   return (
-    <section className="bg-blue-50 px-4 py-10">
-      <div className="container-xl lg:container m-auto">
-        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          Browse Jobs
-        </h2>
-          {/* <!-- Job Listing 1 --> */}
-          {loading ? <Spinner loading={loading}/>
-          : <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              {jobs.map(job => (
-                <Job key={job.id} job={job}/>
-              ))}
-            </div>
-          }
-      </div>
-    </section>
+    <TransitionGroup
+      transitionName="fade"
+      transitionAppear
+      transitionEnter
+      trasitionAppearTimeout={500}
+    >
+      <section className="bg-blue-50 px-4 py-10">
+        <div className="container-xl lg:container m-auto">
+          <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+            Browse Jobs
+          </h2>
+            {/* <!-- Job Listing 1 --> */}
+            {loading ? <Spinner loading={loading}/>
+            : <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {jobs.map(job => (
+                  <Job key={job.id} job={job}/>
+                ))}
+              </div>
+            }
+        </div>
+      </section>
+
+    </TransitionGroup>
   );
 }
 
